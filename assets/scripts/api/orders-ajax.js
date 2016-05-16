48daaf5d4ea06c2204548f94957de8b5e3a56b8b
MichaelB167 <michael.m.bates@gmail.com>
2016-05-16 16:06:08 -0400

    add 'get' ajax requests to orders-ajax

--- assets/scripts/api/orders-ajax.js
+++ assets/scripts/api/orders-ajax.js
@@ -2,16 +2,34 @@
 
 const app = require('./apiurl.js');
 
-const getOrders = () => {
-
+const getOrders = (success, failure) => {
+  $.ajax({
+    method: 'GET',
+    url: app.api + '/orders',
+    dataType: 'json',
+    headers:{
+      Authorization: 'Token token=' + app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
 };
 
-const getOrder = () => {
-
+const getOrder = (success, failure) => {
+  $.ajax({
+    method: 'GET',
+    url: app.api + '/orders/' + app.order._id,
+    dataType: 'json',
+    headers:{
+      Authorization: 'Token token=' + app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
 };
 
 const createOrder = () => {
-
+  
 };
 
 const updateOrder = () => {
