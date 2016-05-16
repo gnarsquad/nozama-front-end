292bf0ce00e9c4da3184346382074154ccb1a273
MichaelB167 <michael.m.bates@gmail.com>
2016-05-16 17:36:37 -0400

    Add success functions for create, update, destroy of orders to orders-ui

--- assets/scripts/api/orders-ui.js
+++ assets/scripts/api/orders-ui.js
@@ -8,12 +8,27 @@ const getOrdersSuccess = (data) => {
 };
 
 const getOrderSuccess = (data) => {
-  app.orders = data.orders;
+  app.order = data.orders;
 };
 
+const createOrderSuccess = (data) => {
+  app.order = data.order;
+};
 
+const updateOrderSuccess = (data) => {
+  console.log(data);
+  console.log('successfully updated order');
+};
+
+const deleteOrderSuccess = () => {
+  app.order = null;
+  console.log('successfully deleted order');
+};
 
 module.exports = {
   getOrdersSuccess,
-  getOrderSuccess
+  getOrderSuccess,
+  createOrderSuccess,
+  updateOrderSuccess,
+  deleteOrderSuccess
 };
