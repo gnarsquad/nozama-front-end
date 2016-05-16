9ede2c97a65ae68933b878346c67b1051dc1b8f9
MichaelB167 <michael.m.bates@gmail.com>
2016-05-16 18:54:09 -0400

    Add ajax functions for orders into main ajax file

--- assets/scripts/api/ajax.js
+++ assets/scripts/api/ajax.js
@@ -144,6 +144,72 @@ const deleteCart = (success, failure) => {
 // ORDERS AJAX STARTS
 
 
+const getOrders = (success, failure) => {
+  $.ajax({
+    method: 'GET',
+    url: app.api + '/orders',
+    dataType: 'json',
+    headers:{
+      Authorization: 'Token token=' + app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+const getOrder = (success, failure) => {
+  $.ajax({
+    method: 'GET',
+    url: app.api + '/orders/' + app.order._id,
+    dataType: 'json',
+    headers:{
+      Authorization: 'Token token=' + app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+const createOrder = (success, failure, data) => {
+  $.ajax({
+    method: "POST",
+    url: app.api + '/orders',
+    dataType: 'json',
+    headers: {
+      Authorization: 'Token token='+ app.user.token,
+    },
+    data,
+  })
+  .done(success)
+  .fail(failure);
+};
+
+const updateOrder = (success, failure, data) => {
+  $.ajax({
+    method: "PATCH",
+    url: app.api + '/orders/' + app.order._id,
+    data,
+    headers: {
+      Authorization: 'Token token='+ app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+const deleteOrder = (success, failure) => {
+  $.ajax({
+    url: app.api + '/orders/' + app.order._id,
+    method: "DELETE",
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+// ORDERS AJAX ENDS
 
 module.exports = {
   signUp,
