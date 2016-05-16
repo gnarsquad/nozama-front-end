321959ab6c5b34326de4990278fe56dabddbd292
sjf125 <sjf125@gmail.com>
2016-05-16 13:12:00 -0400

    Start adding handlebars

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,6 +6,28 @@ $('.product-tile').on("click", function(){
     $('#productModal').modal();
 });
 
+const displayProducts = function(products){
+  const productsTemplate = require('./templates/product-listing.handlebars');
+  $('.content').append(productsTemplate({products}));
+  // $(".gallery-product").lazyload({
+  //   effect : "fadeIn"
+  // });
+  // $('.product-tile').on("click", function(){
+  //   $('#productModal').modal();
+  //   getFlag($(this).data("id"));
+  // });
+};
+
+const getProducts = function(){
+  $.ajax({
+    url: app.api + "/products",
+    // method: 'GET',
+    // dataType: 'json'
+  }).done(function(data){
+    displayProducts(data.products);
+  });
+};
+
 $(() => {
   events.addHandlers();
 });
