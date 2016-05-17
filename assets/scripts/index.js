96105a0209ead3f28599ef0ec5961a86901fd287
sjf125 <sjf125@gmail.com>
2016-05-17 10:38:11 -0400

    Add logic to add to cart button
    Will add to cart if current product is not present in cart
    Will update quantity if current product is present in cart
    
    Ajax not in place yet, waiting for routes to be set up

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,26 +2,45 @@
 
 const events = require('./api/events.js');
 const app = require('./api/apiurl.js');
-// const authUi = require('./api/ui.js');
+const authUi = require('./api/ui.js');
+const authApi = require('./api/ajax.js');
 
-  const checkCart = function(cart, productid) {
+  const checkCart = function(cart, product) {
+    let inCart = 0;
     for (var i = 0; i < cart.length; i++) {
-      if(cart[i].productid === productid) {
+      if(cart[i].productid === product._id) {
         console.log('got a match!');
+        inCart = parseInt(cart[i].quantity);
+        console.log(inCart);
         break;
       }
     }
+    $('#cart-add').on('click', function (event) {
+      let id = product._id;
+      let name = product.name;
+      let price = product.price;
+      let qty = parseInt($('#quantity-select').val()) + inCart;
+      event.preventDefault();
+      console.log(id + ' ' + name + ' ' + price + '  qty: ' + qty);
+      if(inCart === 0) {
+        console.log('add to cart!');
+        // authApi.cartAdd(authUi.success, authUi.failure, id, name, price, qty);
+      } else {
+        console.log('update cart!');
+        // authApi.cartUpdate(authUi.success, authUi.failure, id, name, price, qty);
+      }
+    });
   };
 
 const displayProduct = function(product){
   const display = require('./templates/product.handlebars');
   $('.product-display').empty();
   $('.product-display').append(display({product}));
-  $('#cart-add').attr("prod-id", product._id);
-  $('#cart-add').attr("prod-name", product.name);
-  $('#cart-add').attr("prod-price", product.price);
+  // $('#cart-add').attr("prod-id", product._id);
+  // $('#cart-add').attr("prod-name", product.name);
+  // $('#cart-add').attr("prod-price", product.price);
   console.log(app.user.cart);
-  checkCart(app.user.cart, product._id);
+  checkCart(app.user.cart, product);
 };
 
 const getProduct = function(id){
