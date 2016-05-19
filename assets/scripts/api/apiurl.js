313d19a52a134e1e8b5313af7eed92333686e949
sjf125 <sjf125@gmail.com>
2016-05-18 21:32:31 -0400

    Change api url to heroku

--- assets/scripts/api/apiurl.js
+++ assets/scripts/api/apiurl.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  api: 'http://localhost:3000'
-  // api: 'https://nozama-api.herokuapp.com'
+  // api: 'http://localhost:3000'
+  api: 'https://nozama-api.herokuapp.com'
 };
 
 module.exports = app;
